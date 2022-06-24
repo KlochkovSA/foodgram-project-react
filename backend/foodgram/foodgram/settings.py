80d6a8ebef17b0bde3fc729af3c7e8897eb41ef3
KlochkovSA <sergeyalex1811@yandex.ru>
2022-06-24 20:35:15 +0400

    changed django static url

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -123,8 +123,8 @@ DJOSER = {
     }
 }
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/api_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'api_media')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/api_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'api_static')
