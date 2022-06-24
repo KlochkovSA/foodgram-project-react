33395cd2a5354f6b46bf856ef1a130406180db88
KlochkovSA <sergeyalex1811@yandex.ru>
2022-06-25 00:36:17 +0400

    added name of test database

--- backend/foodgram/foodgram/settings_for_tests.py
+++ backend/foodgram/foodgram/settings_for_tests.py
@@ -1,3 +1,6 @@
 from .settings import *
 
-DATABASES['default'] = {'ENGINE': 'django.db.backends.sqlite3'}
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+}
